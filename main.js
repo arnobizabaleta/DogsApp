2792f8252463619c22511b065b2597f6add07dd7
Arnobi Zabaleta <arnobizabaleta@gmail.com>
2022-05-23 23:34:43 -0500

    Reto completado: Aplicar consumo con async await y agregar un button para cambiar la img mostrada, sin recargar la página

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-console.log("hello world");
 const URL = "https://api.thedogapi.com/v1/images/search";
 
-fetch(URL)
+/*fetch(URL)
     .then(res => res.json()) //Nos devuelve otra promise
     .then(data =>{//Nos devulve los datos solicitados desde la API
         const img = document.querySelector("img");//Tomando el nodo Img de HTML
         img.src = data[0].url;//Asignando atributo src a la img de HTML
         
     });
+*/
+
+
+
+async function getDog() {
+   const response = await fetch(URL);
+   const data = await response.json();
+   const img = document.querySelector('img');
+   img.src = data[0].url;
+}
+const button = document.getElementById('anotherImg');
+button.onclick = getDog;

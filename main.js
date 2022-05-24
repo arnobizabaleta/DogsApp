3286df63fa28b5e9f0792b8289471fd75e4b6794
Arnobi Zabaleta <arnobizabaleta@gmail.com>
2022-05-24 10:06:20 -0500

    Endpoints y query parameters

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.thedogapi.com/v1/images/search";
+const API_URL = "https://api.thedogapi.com/v1/images/search?limit=3";
 
 /*fetch(URL)
     .then(res => res.json()) //Nos devuelve otra promise
@@ -13,8 +13,15 @@ const API_URL = "https://api.thedogapi.com/v1/images/search";
 async function getAnotherDog() {
    const response = await fetch(API_URL);
    const data = await response.json();
-   const img = document.querySelector('img');
-   img.src = data[0].url;
+   
+   console.log(data);
+   const img1 = document.getElementById('img1');
+   const img2 = document.getElementById('img2');
+   const img3 = document.getElementById('img3');
+   
+   img1.src = data[0].url;
+   img2.src = data[1].url;
+   img3.src = data[2].url;
 }
 /* const button = document.getElementById('anotherImg');
 button.onclick = getAnotherDog; */

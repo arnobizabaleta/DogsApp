f1dd51594a9af7375ee2247c70212cc362342708
Arnobi Zabaleta <arnobizabaleta@gmail.com>
2022-05-26 10:30:18 -0500

    Cambiando todo lo necesario de cats a dogs en los archivos de trabajo

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const spanError = document.getElementById("error");
 */
 
 
-async function loadAnotherCats() {
+async function loadAnotherDogs() {
    const response = await fetch(API_URL_RANDOM);
    const data = await response.json();
    /* console.log("RANDOMS");
@@ -38,13 +38,13 @@ async function loadAnotherCats() {
     img1.src = data[0].url;
     img2.src = data[1].url;
    
-    btn1.onclick = ()=> saveFavoriteCat(data[0].id); //Arrow functions par evitar defnir por defecto el valor de onclick al btn
-    btn2.onclick = ()=> saveFavoriteCat(data[1].id);//Así evitmaos que siempre se llame a la funcion saveFavorite por cada img
+    btn1.onclick = ()=> saveFavoriteDog(data[0].id); //Arrow functions par evitar defnir por defecto el valor de onclick al btn
+    btn2.onclick = ()=> saveFavoriteDog(data[1].id);//Así evitmaos que siempre se llame a la funcion saveFavorite por cada img
   }
   
 }
 
-async function loadFavoriteCats() {
+async function loadFavoriteDogs() {
     const response = await fetch(API_URL_FAVORITES,{
         method:"GET",
         headers:{
@@ -60,14 +60,14 @@ async function loadFavoriteCats() {
         
         spanError.innerHTML = "Hubo un error! " + response.status  + " " +data.message;
       }else{
-            const section = document.getElementById('favoriteMichis');
+            const section = document.getElementById('favoriteDogs');
             section.innerHTML  = ""; //Limpiando la sección fav
             const h2 = document.createElement("h2");
             const h2Text = document.createTextNode("Perritos favoritos");
             h2.appendChild(h2Text);
             section.appendChild(h2);
             
-            data.forEach(cat => {
+            data.forEach(dog => {
             
             const article = document.createElement('article');
             article.class = "FavoriteImg";
@@ -78,8 +78,8 @@ async function loadFavoriteCats() {
             
 
             btn.appendChild(btnText);//Introduciendo el texto btnText al button
-            btn.onclick = () => deleteFavoriteCat(cat.id);
-            img.src = cat.image.url; //Agregandole a la img el atributo src del objeto cat atributo cat.image.url
+            btn.onclick = () => deleteFavoriteDog(dog.id);
+            img.src = dog.image.url; //Agregandole a la img el atributo src del objeto cat atributo cat.image.url
             
             
             article.appendChild(img);//Insertando img al article
@@ -93,7 +93,7 @@ async function loadFavoriteCats() {
     
  }
 
- async function saveFavoriteCat(id) {
+ async function saveFavoriteDog(id) {
     //Axios
     const { data, status } = await api.post('/favourites', {
         image_id: id,
@@ -118,19 +118,19 @@ async function loadFavoriteCats() {
         spanError.innerHTML = "Hubo un error! " + status  + " " +data.message;
       }else{
         console.log("Perro guardado en favoritos");
-        loadFavoriteCats(); // Cargando michis favoritos (Update info)
+        loadFavoriteDogs(); // Cargando dogs favoritos (Update info)
         
          const article = document.getElementById("container-img-perritosRandom");
          const span = document.createElement("span");
          span.innerHTML = "Tu perrito ha sido agregado con exito a favoritos,check below!";
          article.appendChild(span);//Insertando article a la section
-         setTimeout(messageDelete,10000,span,article);
+         setTimeout(messageDelete,7000,span,article);
     }
   
     
  }
 
- async function deleteFavoriteCat(id) {
+ async function deleteFavoriteDog(id) {
     const response = await fetch(API_URL_FAVORITES_DELETE(id),{
         method: 'DELETE',
         headers:{
@@ -144,13 +144,13 @@ async function loadFavoriteCats() {
     if(response.status !== 200){
         spanError.innerHTML = "Hubo un error! " + response.status  + " " +data.message;
       }else{
-        console.log("Michi elimiando de favoritos");
-        loadFavoriteCats(); // Cargando michis favoritos (Update info)
+        console.log("Perro elimiando de favoritos");
+        loadFavoriteDogs(); // Cargando dogs favoritos (Update info)
       }
     
  }
  
- async function uploadMichiPhoto() {
+ async function uploadDogPhoto() {
     const form = document.getElementById("uploadingForm");
     const formData = new FormData(form)//agregando todos los valores input de nuestro form a la instancia FormDATA
     console.log(formData.get("file"))
@@ -167,15 +167,15 @@ async function loadFavoriteCats() {
         console.log('Error')
         console.log({data})
     }else {
-        console.log('Foto de Michi subida')
+        console.log('Foto de Perrito subida')
         console.log({ data })
         console.log(data.url)
-        saveFavoriteCat(data.id)//para agregar el michi cargado a favoritos.
-        const section = document.getElementById("uploadMichi");
+        saveFavoriteDog(data.id)//para agregar el michi cargado a favoritos.
+        const section = document.getElementById("uploadDog");
         const span = document.createElement("span");
         span.innerHTML = "Tu perrito ha sido cargado y agregado con exito a favoritos,check below!";
         section.appendChild(span);//Insertando article a la section
-        setTimeout(messageDelete,10000,span,section);
+        setTimeout(messageDelete,7000,span,section);
         console.log('Fin de la función')
         
     }
@@ -187,8 +187,8 @@ async function loadFavoriteCats() {
  
 /* const button = document.getElementById('anotherImg');
 button.onclick = getAnotherDog; */
-loadAnotherCats(); //Llamando a la funcion cuando cargamos nuestro codigo
-loadFavoriteCats();
+loadAnotherDogs(); //Llamando a la funcion cuando cargamos nuestro codigo
+loadFavoriteDogs();
 
 function messageDelete (span,section) {
     span.innerHTML = "";
